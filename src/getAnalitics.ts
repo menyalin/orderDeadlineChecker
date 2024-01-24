4339f67f2d76ed4776a763968bad79b6023dbf54
Menyalin Alexey <menyalin@icloud.com>
2024-01-24 16:42:12 +0300

    data updated

--- src/getAnalitics.ts
+++ src/getAnalitics.ts
@@ -5,7 +5,7 @@ import { InvoiceRepo } from './repositories'
 
 async function getData() {
   const startDate = new Date('2023-07-01')
-  const endDate = new Date('2023-12-01')
+  const endDate = new Date('2024-01-01')
   await dataSource.initialize()
   const rawData = await InvoiceRepo.getAnaliticsDataByPeriod(startDate, endDate)
   const analiticItems: AnaliticItem[] = rawData.map(i => new AnaliticItem(i))
